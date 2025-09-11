17a9d73c787e5b121c5e2a7dca3f7f43989e0941
Frankie <1426203851@qq.com>
2025-09-12 03:07:15 +0800

    chore: replace rolldown-vite with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'rolldown-vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProd = mode === 'production'
-  return {
-    base: isProd ? '/superellipse-generator/' : '/',
-    server: {
-      port: 3000,
-      open: true,
+export default defineConfig({
+  base: './',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    build: {
-      outDir: 'dist',
-      sourcemap: true,
-    },
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
-    },
-  }
+  },
 })
