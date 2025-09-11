32af7a4e9abe053f0981829f873234dad2cc2ecb
Frankie <1426203851@qq.com>
2025-09-11 17:01:33 +0800

    ci: add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,18 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'rolldown-vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production'
+  return {
+    base: isProd ? '/superellipse/' : '/',
+    server: {
+      port: 3000,
+      open: true,
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+    },
+    plugins: [react()],
+  }
 })
